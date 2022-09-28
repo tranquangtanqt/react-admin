c96cafd27e698dfcd8998a642a49e9e3f71c66bd
tranquangtanqt <tranquangtanqt1990@gmail.com>
2022-09-28 22:04:20 +0700

    update todo edtail

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const AxiosClient = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  //baseURL: "http://localhost:8081/api",
+  // baseURL: "http://localhost:8081/api",
   headers: {
     "content-type": "application/json",
   },
