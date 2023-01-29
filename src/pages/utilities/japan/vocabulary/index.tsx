f112031c6c9afc47f75cb67a9790262b7705cfe3
tranquangtanqt <tranquangtanqt1990@gmail.com>
2023-01-29 16:25:09 +0700

    japan - vocabulary

--- src/pages/utilities/japan/vocabulary/index.tsx
+++ src/pages/utilities/japan/vocabulary/index.tsx
@@ -18,6 +18,7 @@ export const UtilitiesJapanVocabulary = () => {
   });
   const [checked, setChecked] = useState<string[]>([]);
   const [level, setLevel] = useState("N5");
+  const [questionTotal, setQuestionTotal] = useState("4");
 
   /**
    * Add/Remove checked item from list
@@ -48,14 +49,19 @@ export const UtilitiesJapanVocabulary = () => {
   const openUnitPage = () => {
     localStorage.setItem("japan-unit", JSON.stringify(checked));
     localStorage.setItem("japan-level", level);
+    localStorage.setItem("japan-question-total", questionTotal);
     navigate("/utilities/japan/vocabulary/unit");
   };
+
+  const handleSelectQuestionTotal = (value: string) => {
+    setQuestionTotal(value);
+  }
 
   return (
     <>
       <PageTitle title="Từ vựng"></PageTitle>
       <div className="row mt-2">
-        <div className="col-12 col-sm-12 col-md-12">
+        <div className="col-6 col-sm-6 col-md-6">
           <b>1. Chọn cấp độ</b>
           <select className="form-select form-select-sm" defaultValue={"N5"} onChange={(e) => handleSelectLevel(e.target.value)}>
             <option value="N5">N5</option>
@@ -63,29 +69,40 @@ export const UtilitiesJapanVocabulary = () => {
             <option value="N3">N3</option>
           </select>
         </div>
+
+        <div className="col-6 col-sm-6 col-md-6">
+          <b>2. Chọn tổng số đáp án</b>
+          <select className="form-select form-select-sm" defaultValue={"4"} onChange={(e) => handleSelectQuestionTotal(e.target.value)}>
+            <option value="4">4</option>
+            <option value="6">6</option>
+            <option value="8">8</option>
+          </select>
+        </div>
       </div>
 
       <div className="row mt-2">
-        <div className="col-12 col-sm-12 col-md-12">
+        
           {units.map(
             (item, index) =>
               item.isShow === 1 && (
-                <div key={index} className="form-check">
-                  <input
-                    value={item.unit}
-                    type="checkbox"
-                    className="form-check-input"
-                    id={"checkbox-unit-" + index}
-                    onChange={(e) => handleCheck(e)}
-                  />
-                  <label className="form-check-label" htmlFor={"checkbox-unit-" + index}>
-                    {item.unitName} - {item.level}
-                  </label>
-                  <span></span>
+                <div  key={index} className="col-4 col-sm-4 col-md-4">
+                  <div className="form-check">
+                    <input
+                      value={item.unit}
+                      type="checkbox"
+                      className="form-check-input"
+                      id={"checkbox-unit-" + index}
+                      onChange={(e) => handleCheck(e)}
+                    />
+                    <label className="form-check-label" htmlFor={"checkbox-unit-" + index}>
+                      {item.unitName}
+                    </label>
+                    <span></span>
+                  </div>
                 </div>
               )
           )}
-        </div>
+        {/* </div> */}
       </div>
 
       <div className="row mt-2">
---
+++ src/pages/utilities/japan/vocabulary/unit/index.tsx
@@ -0,0 +1,320 @@
+import { Fragment, useEffect, useState } from "react";
+import { PageTitle } from "../../../../../components/modules/pageTitle";
+import NumberUtils from "../../../../../utils/NumberUtils";
+import { useNavigate } from "react-router-dom";
+import dataN5 from "../dataN5";
+import "./unit.css";
+
+export const UtilitiesJapanVocabularyUnit = () => {
+  const navigate = useNavigate();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (units === null) {
+      navigate("/utilities/japan/vocabulary");
+    }
+    // eslint-disable-next-line
+  }, [navigate]);
+
+  /**
+   * Mang chua toan bo cac bai hoc
+   */
+  const [units] = useState(() => initUnit());
+  function initUnit() {
+    let localValue = localStorage.getItem("japan-unit");
+    let updateUnit = null;
+    if (localValue !== null) {
+      updateUnit = JSON.parse(localValue);
+    }
+    localStorage.removeItem("japan-unit");
+
+    return updateUnit;
+  }
+
+  /**
+   * Cap do
+   */
+  const [level] = useState(() => initLevel());
+  function initLevel() {
+    let result = localStorage.getItem("japan-level");
+    localStorage.removeItem("japan-level");
+    return result;
+  }
+
+  /**
+   * Tong so cau hoi
+   */
+  const [questionTotal] = useState(() => initQuestionTotal());
+  function initQuestionTotal() {
+    let total = localStorage.getItem("japan-question-total");
+    localStorage.removeItem("japan-question-total");
+    return Number(total);
+  }
+
+  /**
+   * Chi so dap an chinh xac trong mang chua cau tra loi (questions)
+   */
+  const [answerCorrect, setAnswerCorrect] = useState(() => initAnswerCorrect());
+  function initAnswerCorrect() {
+    return NumberUtils.getRandomInt(questionTotal);
+  }
+
+  /**
+   * Mang chua tat ca cac tu vung
+   */
+  function initVocabulary() {
+    let arr: any = [];
+    if (level === "N5") {
+      for (let unitName of units) {
+        let unit = dataN5.get(unitName);
+        unit.forEach((item: any) => {
+          arr.push(item);
+        });
+      }
+      return arr;
+    }
+  }
+
+  /**
+   * Mang chua cau tra loi
+   */
+  const [questions, setQuestions] = useState(() => initQuestion());
+  function initQuestion() {
+    let vocabulary = initVocabulary();
+    let result = [];
+    for (let i = 0; i < questionTotal; i++) {
+      let length = vocabulary.length;
+      let rd = NumberUtils.getRandomInt(length);
+      result.push(vocabulary[rd]);
+      vocabulary.splice(rd, 1);
+    }
+    return result;
+  }
+
+  /**
+   * Hien thi mau nen cho dap an (Dung: xanh la, sai: do)
+   */
+  const [isAnswerCorrect, setIsAnswerCorrect] = useState(false);
+
+  /**
+   * Hien thi cau tra loi (khi nguoi dung chon dap an)
+   */
+  const [isShowAnswer, setIsShowAnswer] = useState(false);
+
+  /**
+   * Tong so cau dung
+   */
+  const [correctNumber, setCorrectNumber] = useState(0);
+
+  /**
+   * Tong so cau sai
+   */
+  const [wrongNumber, setWrongNumber] = useState(0);
+
+  /**
+   * khi da chon cau tra loi => khong cho click tiep dap an
+   */
+  const [isEnableClick, setIsEnableClick] = useState(true);
+
+  /**
+   * Thay doi mau nen khi chon dap an
+   */
+  const [backgroundColor, setBackgroundColor] = useState(() => initBackgroundColor());
+  function initBackgroundColor() {
+    return ["", "", "", "", "", ""];
+  }
+
+  /**
+   * onclick: chon cau tra loi
+   */
+  const answer = (index: number) => {
+    if (!isEnableClick) return;
+    setIsShowAnswer(true);
+    if (index === answerCorrect) {
+      setIsAnswerCorrect(true);
+      setCorrectNumber(correctNumber + 1);
+    } else {
+      setIsAnswerCorrect(false);
+      setWrongNumber(wrongNumber + 1);
+    }
+    setIsEnableClick(false);
+    let updateBackgroundColor = [...backgroundColor];
+    updateBackgroundColor[index] = "choose-answer";
+    setBackgroundColor(updateBackgroundColor);
+  };
+
+  /**
+   * onclick: chon cau hoi tiep theo
+   */
+  const nextQuestion = () => {
+    setIsLoading(true);
+    setIsEnableClick(true);
+    setIsShowAnswer(false);
+    setQuestions(initQuestion());
+    setBackgroundColor(initBackgroundColor());
+    setAnswerCorrect(initAnswerCorrect());
+    setIsLoading(false);
+    if (isEnableClick) {
+      setWrongNumber(wrongNumber + 1);
+    }
+  };
+  return (
+    <>
+      <PageTitle title="Từ vựng"></PageTitle>
+      <div className={isLoading ? "row mt-2" : "row mt-2 d-none"}>
+        <div style={{ minHeight: "30vh" }}>
+          <div className="loading-spinner"></div>
+        </div>
+      </div>
+
+      <div className={!isLoading ? "row mt-2" : "row mt-2 d-none"}>
+        <div className="col-12 col-sm-8 col-md-8">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="card text-white bg-dark mb-2">
+                <div className="card-body padding-custom">
+                  <p className="card-text text-center">
+                    {questions?.slice(answerCorrect, answerCorrect + 1).map((item: any, key: any) => (
+                      <b key={key}>{item.translate}</b>
+                    ))}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {[...Array(questionTotal / 2)].map((value: any, index: any) => (
+            <Fragment key={index}>
+              <div className="row">
+                <div className="col-md-6 col-lg-6 col-xl-6">
+                  <div className="card cursor-pointer mb-2">
+                    <div className={`card-body padding-custom ${backgroundColor[index * 2]}`} onClick={() => answer(index * 2)}>
+                      <p className="card-text text-center">
+                        {questions?.slice(index * 2, index * 2 + 1).map((item: any, key: any) =>
+                          item.kanji !== "" ? (
+                            <b key={key}>
+                              {item.hiragana} ({item.kanji})
+                            </b>
+                          ) : (
+                            <b key={key}>{item.hiragana}</b>
+                          )
+                        )}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-6 col-lg-6 col-xl-6">
+                  <div className="card cursor-pointer mb-2">
+                    <div className={`card-body padding-custom ${backgroundColor[index * 2 + 1]}`} onClick={() => answer(index * 2 + 1)}>
+                      <p className="card-text text-center">
+                        {questions?.slice(index * 2 + 1, index * 2 + 2).map((item: any, key: any) =>
+                          item.kanji !== "" ? (
+                            <b key={key}>
+                              {item.hiragana} ({item.kanji})
+                            </b>
+                          ) : (
+                            <b key={key}>{item.hiragana}</b>
+                          )
+                        )}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Fragment>
+          ))}
+
+          <div className="row">
+            {isShowAnswer && (
+              <Fragment>
+                <div className="col-12 col-md-12 col-lg-12 col-xl-12">
+                  <div className={`card text-white ${isAnswerCorrect ? "bg-success" : "bg-danger"} mb-3`}>
+                    <div className="card-body padding-custom">
+                      <p className="card-text text-center">
+                        {questions?.slice(answerCorrect, answerCorrect + 1).map((item: any, key: any) =>
+                          item.kanji !== "" ? (
+                            <b key={key}>
+                              {item.hiragana} ({item.kanji})
+                            </b>
+                          ) : (
+                            <b key={key}>{item.hiragana}</b>
+                          )
+                        )}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </Fragment>
+            )}
+            <div className="col-4 offset-4 col-md-4 offset-md-4 col-lg-4 offset-lg-4 col-xl-4 offset-xl-4">
+              <div className="card text-white bg-info cursor-pointer mb-3" onClick={() => nextQuestion()}>
+                <div className="card-body padding-custom">
+                  <p className="card-text text-center">
+                    <b>Next</b>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-4 col-lg-4 col-xl-4">
+          <div className="row">
+            <div className="col-md-4 col-lg-4 col-xl-4 col-4">
+              <div className="card text-white bg-success mb-3">
+                <div className="card-body padding-custom">
+                  <p className="card-text text-center">
+                    <b>{correctNumber}</b>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-lg-4 col-xl-4 col-4">
+              <div className="card text-white bg-danger mb-3">
+                <div className="card-body padding-custom">
+                  <p className="card-text text-center">
+                    <b>{wrongNumber}</b>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 col-lg-4 col-xl-4 col-4">
+              <div className="card text-white bg-info mb-3">
+                <div className="card-body padding-custom">
+                  <p className="card-text text-center">
+                    <b>{correctNumber + wrongNumber > 0 ? Math.round((correctNumber / (correctNumber + wrongNumber)) * 100 * 100) / 100 : 0}%</b>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="row d-none d-sm-block">
+            <div className="col-md-12 col-lg-12 col-xl-12">
+              {isShowAnswer && isAnswerCorrect && (
+                <Fragment>
+                  <svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+                    <circle className="checkmark__circle" cx="26" cy="26" r="25" fill="none" />
+                    <path className="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
+                  </svg>
+                </Fragment>
+              )}
+
+              {isShowAnswer && !isAnswerCorrect && (
+                <Fragment>
+                  <div className="swal2-icon swal2-error swal2-animate-error-icon d-flex">
+                    <span className="swal2-x-mark">
+                      <span className="swal2-x-mark-line-left"></span>
+                      <span className="swal2-x-mark-line-right"></span>
+                    </span>
+                  </div>
+                </Fragment>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
