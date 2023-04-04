f96e565a4848811a0d08f13142667befafda8d2c
tantq <tantq@aureole-it.vn>
2023-04-04 18:43:58 +0700

    add index

---
+++ src/pages/programming/php/index.tsx
@@ -0,0 +1,47 @@
+import { PageTitle } from "../../../components/modules/page-title";
+import { CardList } from "../../../components/modules/card-list";
+
+export const ProgrammingPHP = () => {
+  const cardData = [
+    {
+      title: "Convert",
+      links: [
+        {
+          text: "Chuyển đổi Datetime",
+          path: "/programming-php/convert/convert-datetime",
+        },
+        {
+          text: "Chuyển chuỗi Unicode thành chuỗi không dấu",
+          path: "/programming-php/convert/convert-unicode",
+        },
+        {
+          text: "Cắt chuỗi với dấu ba chấm (ellipses)",
+          path: "/programming-php/convert/substring-ellipses",
+        },
+        {
+          text: "Xóa tệp",
+          path: "/programming-php/convert/delete-file",
+        },
+      ],
+    },
+    {
+      title: "Laravel",
+      links: [
+        {
+          text: "Cài đặt font cho dompdf",
+          path: "/programming-php/lavavel/install-font-dompdf",
+        }
+      ],
+    },
+  ];
+  return (
+    <>
+      <>
+      <PageTitle title="PHP"></PageTitle>
+      <div className="row">
+        <CardList cardData={cardData}></CardList>
+      </div>
+    </>
+    </>
+  );
+};
