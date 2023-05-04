fd7685f748d6308487453c48f2693c8b25c55aac
tantq <tantq@aureole-it.vn>
2023-05-04 11:20:11 +0700

    git change

--- src/pages/utilities/general/collapse-git/index.tsx
+++ src/pages/utilities/general/collapse-git/index.tsx
@@ -13,6 +13,26 @@ export const UtilitiesGeneralCollapseGit = () => {
             language="javascript"
           />
         </div>
+        <div className="col-12 col-sm-12 col-md-12">
+          <b>2. </b>
+          <Code
+            code={`function save(text){
+    var link = document.createElement('a');
+    link.href = 'data:text/plain;charset=UTF-8,' + escape(text);
+    link.download = 'output.txt';
+    link.click();
+};
+var fileNameAll = "";
+$(".file-title-name").each(function (i) {
+  //console.log(this.innerHTML);
+  fileNameAll += this.innerHTML + "\\n";
+});
+var arr = fileNameAll.split("\\n");
+arr = arr.filter(item => item.trim() !== "");
+save(arr.join("\\n"));`}
+            language="javascript"
+          />
+        </div>
       </div>
     </>
   );
