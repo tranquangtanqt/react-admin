9163a5bd7cf782b30d9f366cec7e212668dd629e
tantq <tantq@aureole-it.vn>
2023-06-05 18:21:17 +0700

    add postgresql table

--- src/pages/database/postgresql/table/general/index.tsx
+++ src/pages/database/postgresql/table/general/index.tsx
@@ -1,24 +1,15 @@
 import { PageTitle } from '../../../../../components/modules/page-title';
 import Code from '../../../../../components/modules/code';
+import { Fragment } from 'react';
 
 export const PostgresqlTableGeneral = () => {
   const data = [
     {
       title: 'Get size in folder',
-    },
-  ];
-
-  return (
-    <>
-      <PageTitle title="PostgresqlTableGeneral"></PageTitle>
-      <div className="row mt-2">
-        <div className="col-12 col-sm-12 col-md-12">
-          <p>
-            <b>Get size in folder</b>
-          </p>
-          <div className="tab-1">
-            <Code
-              code={`SELECT
+      contents: [
+        {
+          p: null,
+          code: `SELECT
   table_name
   , pg_size_pretty(pg_relation_size(quote_ident(table_name)))
   , pg_relation_size(quote_ident(table_name)) 
@@ -27,11 +18,41 @@ FROM
 WHERE
   table_schema = 'public' 
 ORDER BY
-  3 desc;`}
-              language="sql"
-            />
-          </div>
-        </div>
+  3 desc;`,
+          language: 'sql',
+        },
+      ],
+    },
+  ];
+
+  return (
+    <>
+      <PageTitle title="PostgresqlTableGeneral"></PageTitle>
+      <div className="row mt-2">
+        {data.map((dataVal, dataKey) => (
+          <Fragment>
+            <div className="col-12 col-sm-12 col-md-12" key={dataKey}>
+              <p>
+                <b>
+                  {dataKey + 1}. {dataVal.title}
+                </b>
+              </p>
+              {dataVal.contents.map((contentVal, contentKey) => (
+                <Fragment>
+                  <div className="tab-1">
+                    {contentVal.p && contentVal.p}
+                    {contentVal.code && (
+                      <Code
+                        code={contentVal.code}
+                        language={contentVal.language}
+                      />
+                    )}
+                  </div>
+                </Fragment>
+              ))}
+            </div>
+          </Fragment>
+        ))}
       </div>
     </>
   );
