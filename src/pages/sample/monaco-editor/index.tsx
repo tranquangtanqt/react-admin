c6f623f683289b745e83ff82816aeb6e47b7ebe4
tantq <tantq@aureole-it.vn>
2024-02-21 10:48:12 +0700

    update monaco sample

--- src/pages/sample/monaco-editor/index.tsx
+++ src/pages/sample/monaco-editor/index.tsx
@@ -1,47 +1,61 @@
 import { useMonaco } from '@monaco-editor/react';
 import { PageTitle } from 'components/modules/page-title';
+import { useEffect, useRef } from 'react';
 
 export const SampleMonacoEditorPage = () => {
+  const refEditor = useRef<HTMLDivElement>(null);
+
   const monaco = useMonaco();
-  if (monaco) {
-    const originalModel = monaco.editor.createModel(
-      'heLLo world!',
-      'text/plain',
-    );
 
-    const modifiedModel = monaco.editor.createModel(
-      'hello orlando!',
-      'text/plain',
-    );
+  useEffect(() => {
+    if (refEditor.current) {
+      if (monaco) {
+        const originalModel = monaco.editor.createModel(
+          'heLLo world!',
+          'text/plain',
+        );
 
-    const diffEditor = monaco.editor.createDiffEditor(
-      document.getElementById('container-editor') as HTMLElement,
-      {
-        originalEditable: true, // for left pane
-        readOnly: false, // for right pane
-      },
-    );
+        const modifiedModel = monaco.editor.createModel(
+          'hello orlando!',
+          'text/plain',
+        );
 
-    diffEditor.setModel({
-      original: originalModel,
-      modified: modifiedModel,
-    });
+        if (refEditor.current) {
+          const diffEditor = monaco.editor.createDiffEditor(
+            document.getElementById('container-editor') as HTMLElement,
+            {
+              originalEditable: true, // for left pane
+              readOnly: false, // for right pane
+            },
+          );
 
-    if (diffEditor.getModel()) {
-      monaco.editor.setModelLanguage(
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        diffEditor.getModel()!.original,
-        'javascript',
-      );
+          diffEditor.setModel({
+            original: originalModel,
+            modified: modifiedModel,
+          });
+
+          if (diffEditor.getModel()) {
+            monaco.editor.setModelLanguage(
+              // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+              diffEditor.getModel()!.original,
+              'javascript',
+            );
+          }
+        }
+      }
     }
-  }
+  }, [monaco]);
 
   return (
     <>
       <PageTitle title="Monaco Editor Sample"></PageTitle>
       <div className="row mt-2">
         <div className="col-12 col-sm-12 col-md-12">
-          <div id="container-editor" style={{ height: '90vh' }}></div>
+          <div
+            id="container-editor"
+            style={{ height: '70vh' }}
+            ref={refEditor}
+          ></div>
         </div>
       </div>
     </>
