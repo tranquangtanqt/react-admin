e0ce803e1389161e3c7d2b5ad22e59e019f9321e
tranquangtanqt <tranquangtanqt1990@gmail.com>
2024-04-14 12:49:06 +0700

    delete file in src\programming

--- src/pages/programming/php/content/index.tsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from 'react-router-dom';
-import { mapContent } from '../data';
-
-export const ProgrammingPHPContent = () => {
-  const params = useParams();
-  const content: string = params.content || '';
-  return <>{mapContent.get(content)}</>;
-};
